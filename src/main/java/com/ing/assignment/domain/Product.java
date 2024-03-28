b90ae503b8f21c4e6098be32148e2eb4acb92e7b
neacsiuandrei <andrey.andreyQ@yahoo.com>
2024-03-28 09:17:18 +0200

    Adding product domain.

---
+++ src/main/java/com/ing/assignment/domain/Product.java
@@ -0,0 +1,50 @@
+package com.ing.assignment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private float price;
+
+    public Product() {
+
+    }
+
+    public Product(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
